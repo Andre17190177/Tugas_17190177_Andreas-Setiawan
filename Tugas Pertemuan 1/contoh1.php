544129d2413b7d3d4ed5be20eb3239b60599ef51
Andre17190177 <andreas1439setiawan@gmail.com>
2022-03-20 17:48:59 +0700

    Upload tugas Pertemuan 1

---
+++ Tugas Pertemuan 1/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Andreas Setiawan <br>
+              Saya tinggal di daerah Ciledug <br>
+              Olahraga yang saya sukai adalah berenang";
+    }
+}
