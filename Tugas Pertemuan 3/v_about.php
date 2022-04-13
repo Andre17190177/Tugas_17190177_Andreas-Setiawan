8e505f6b30c35133ae6973965cc49b7442b98cd1
Andre17190177 <andreas1439setiawan@gmail.com>
2022-04-13 16:05:15 +0700

    Upload About

---
+++ Tugas Pertemuan 3/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Andreas</li>
+        <li>Nama Belakang : Setiawan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Griya Kencana 2</li>
+    </ul>
+    <br>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+    <br>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Basket</li>
+    </ul>
+</section>
